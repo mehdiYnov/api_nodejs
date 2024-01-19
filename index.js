365bc579db42b350b88e61c985b02c38d37c7d75
kachourmehdi <156905628+mehdiYnov@users.noreply.github.com>
2024-01-19 11:17:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.put("/wydads/:id", (req, res) => {
 //   res.status(200).json(wydads);
 // });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("looool");
 });
