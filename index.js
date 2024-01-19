c44263d7003ac2275708ad2c3cb876eca51f70ec
kachourmehdi <125712746+KACHOURElmehdi@users.noreply.github.com>
2024-01-19 02:17:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const wydads = require("./data/wydad.json");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+// app.get('/wydads', (req, res) =>{
+//     res.status(200).json(wydads)
+// })
+
+// app.get('/wydad/:id', (req, res) =>{
+//     const id = parseInt(req.params.id)
+//     const wydad = wydads.find(wydad => wydad.id === id)
+//     res.status(200).json(wydad)
+// })
+
+// app.post("/wydads", (req, res) => {
+//     console.log(req.body)
+//     wydads.push(req.body)
+//     res.status(201).json()
+// })
+
+// app.get('/wydads', (req, res ) =>{
+//     res.status(200).json(wydads)
+// })
+
+app.put("/wydads/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const wydad = wydads.find((wydad) => wydad.id === id);
+
+  if (!wydad) {
+    return res.status(404).json({ error: 'Wydad not found' });
+}   
+
+  wydad.matr = req.body.matr;
+  wydad.date = req.body.date;
+
+  fs.writeFileSync('./data/wydad.json', JSON.stringify(wydads, null, 2));
+
+  res.status(303).json(wydads);
+});
+
+// app.delete("/wydads/:id", (req, res) => {
+//   const id = parseInt(req.params.id);
+//   const wydad = wydads.find((wydad) => wydad.id === id);
+
+//   wydads.splice(wydads.indexOf(wydad), 1);
+//   fs.writeFileSync("./data/wydad.json", JSON.stringify(wydads, null, 2));
+
+//   res.status(200).json(wydads);
+// });
+
+app.listen(8080, () => {
+  console.log("looool");
+});
