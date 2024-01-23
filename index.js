cd94bc8e71343c3f31fb99ed7cf1caace45a709e
kachourmehdi <156905628+mehdiYnov@users.noreply.github.com>
2024-01-23 09:44:05 +0100

    connection

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
+require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const app = express();
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Create a MySQL connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "apiYnov",
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
 });
-
-// Connect to MySQL
-connection.connect();
 
 // Define a route to retrieve data from the MySQL database
 app.get("/wydads", (req, res) => {
@@ -33,13 +29,77 @@ app.get("/wydads", (req, res) => {
   });
 });
 
-// Close the MySQL connection when the Node.js app is closed
-process.on("SIGINT", () => {
-  connection.end();
-  process.exit();
+
+
+app.get("/wydads/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const query = "SELECT * FROM wydadinfo WHERE id = " + id;
+
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Error retrieving data from MySQL");
+    } else {
+      // Send the retrieved data as a JSON response
+      res.status(200).json(results);
+    }
+  });
 });
 
-const PORT = process.env.PORT || 43334;
+
+
+app.delete("/wydads/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const query = "delete FROM wydadinfo WHERE id = " + id;
+
+  connection.query(query, (error, results, fields) => {
+    if (!error) {
+      // Send the retrieved data as a JSON response
+      // res.status(200).json(results);
+      res.status(200).send("You was Drop the line " + id + " from your database");
+    } else {
+      console.error(error);
+      res.status(500).send("Error retrieving data from MySQL");
+    }
+  });
+});
+
+
+app.post("/wydads/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  
+  // Check if the record with the specified ID exists
+  const checkQuery = "SELECT * FROM wydadinfo WHERE id =" + id;
+  connection.query(checkQuery, (error, results, fields) => {
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'Wydad not found' });
+    }
+
+    // Update the record
+    const updateQuery = "UPDATE wydadinfo SET matr = ?, date = ? WHERE id = ?";
+    const values = [req.body.matr, req.body.date, id];
+
+    connection.query(updateQuery, values, (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send("Error updating data in MySQL");
+      }
+
+      res.status(201).send("You have updated the line " + id + " in your database");
+    });
+  });
+});
+
+
+
+
+pool.getConnection((err, conn) => {
+  if (err) console.log(err);
+  console.log("Connected successfully");
+});
+
+const PORT = process.env.PORT || "3306";
 app.listen(PORT, () => {
   console.log("looool");
 });
+module.exports = pool.promise();
